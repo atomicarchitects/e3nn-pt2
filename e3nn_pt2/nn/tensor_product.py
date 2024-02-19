bc528c43d22f2c708e404bf8d8c92267f16f6ccf
Mit <53411468+mitkotak@users.noreply.github.com>
2024-02-19 15:30:38 -0500

    initializing output tensor before runtime

--- e3nn_pt2/nn/tensor_product.py
+++ e3nn_pt2/nn/tensor_product.py
@@ -11,13 +11,13 @@ torch.set_float32_matmul_precision('high')
 
 class TensorProduct(torch.nn.Module):
 
-    def __init__(self, irreps_in1, irreps_in2, device="cpu"):
+    def __init__(self, irreps_in1, irreps_in2, batch=1, device="cpu"):
         super().__init__()
         self.irreps_out = so3.Irreps(o3.FullTensorProduct(irreps_in1, irreps_in2).irreps_out.__str__())
         self.cg = so3.clebsch_gordan(irreps_in1.lmax, irreps_in2.lmax, self.irreps_out.lmax).to(device=device)
         self.pseudo_tensor_in1 = irreps_in1.parity_dim == 2
         self.pseudo_tensor_in2 = irreps_in2.parity_dim == 2
-        
+    
         # Check if number of channels is same for the 2 inputs
         # else use the channel axis from the first input
         # typically useful for spherical type opertions
@@ -26,34 +26,34 @@ class TensorProduct(torch.nn.Module):
         if irreps_in1.mul_dim == irreps_in2.mul_dim :
             self.channel_dim = 'f' # f x f -> f
         else:
-            self.channel_dim = 'g' # f x g -> f 
+            self.channel_dim = 'g' # f x g -> f
+
+        self.output = torch.empty(batch,
+                                  2 if (self.pseudo_tensor_in1 or self.pseudo_tensor_in2) else 1,
+                                  (self.irreps_out.lmax + 1)**2,
+                                  irreps_in1.mul_dim).to(device=device)
         
     @torch.compile(fullgraph=True)
     def forward(self, x1, x2):
         if (not self.pseudo_tensor_in1) and (not self.pseudo_tensor_in2):
-            return torch.einsum(f'...lf, ...m{self.channel_dim}, lmn-> ...nf', x1, x2, self.cg)
+            self.output[:, 0:1, :, :] = torch.einsum(f'...lf, ...m{self.channel_dim}, lmn-> ...nf', x1, x2, self.cg)
 
-        def _couple_slices(i: int, j: int):
-            return torch.einsum(
-                f'...lf, ...m{self.channel_dim}, lmn -> ...nf',
-                x1[..., i, :, :],
-                x2[..., j, :, :],
-                self.cg,
-            )
-        
-        even_parity_output = odd_parity_output = None
-        if (not self.pseudo_tensor_in1) and (not self.pseudo_tensor_in2):
-            eee = _couple_slices(0, 0) # even + even -> even
-            even_parity_output = eee
-        if (self.pseudo_tensor_in1) and (not self.pseudo_tensor_in2):
-            oeo = _couple_slices(1, 0)  # odd + even -> odd
-            odd_parity_output += oeo
-        if (self.pseudo_tensor_in1) and (self.pseudo_tensor_in2):
-            ooe = _couple_slices(1, 1)  # odd + odd -> even
-            even_parity_output += ooe
-        if (not self.pseudo_tensor_in1) and (self.pseudo_tensor_in2):
-            eoo = _couple_slices(0, 1)  # even + odd -> odd
-            odd_parity_output += eoo
+        else:
+            def _couple_slices(i: int, j: int):
+                return torch.einsum(
+                    f'...lf, ...m{self.channel_dim}, lmn -> ...nf',
+                    x1[..., i, :, :],
+                    x2[..., j, :, :],
+                    self.cg,
+                )
+            
+            if (not self.pseudo_tensor_in1) and (not self.pseudo_tensor_in2):
+                self.output[:, 0:1, :, :] += _couple_slices(0, 0) # even + even -> even
+            if (self.pseudo_tensor_in1) and (not self.pseudo_tensor_in2):
+                self.output[:, 1:2, :, :] += _couple_slices(1, 0)  # odd + even -> odd
+            if (self.pseudo_tensor_in1) and (self.pseudo_tensor_in2):
+                self.output[:, 0:1, :, :] += _couple_slices(1, 1)  # odd + odd -> even
+            if (not self.pseudo_tensor_in1) and (self.pseudo_tensor_in2):
+                self.output[:, 1:2, :, :] += _couple_slices(0, 1)  # even + odd -> odd
 
-        # Combine same parities and return stacked features.
-        return torch.stack((even_parity_output, odd_parity_output), axis=-3)
+        return self.output
--- e3nn_pt2/nn/tplinear.py
+++ e3nn_pt2/nn/tplinear.py
@@ -9,11 +9,12 @@ class TensorProductLinear(torch.nn.Module):
     def __init__(self,
                 irreps_in1: so3.Irreps,
                 irreps_in2: so3.Irreps,
+                batch: int=1,
                 device: str="cpu",
                 use_bias: Optional[int] = False):
         
         super().__init__()
-        self.tp = nn.TensorProduct(irreps_in1, irreps_in2, device)
+        self.tp = nn.TensorProduct(irreps_in1, irreps_in2, batch, device)
         # Assuming that the first input has the channels
         self.linear = nn.Linear(irreps_in=self.tp.irreps_out, irreps_out=irreps_in1, device=device)           
 
